ee5d4fa5628bc382853ab2a9ed38bd33c35a3ffc
walera01 <wladislaw1004@gmail.com>
2022-08-18 17:53:11 +0300

    Создана модель добавления в бд параметров заказа+

--- PSstore/store/views.py
+++ PSstore/store/views.py
@@ -45,14 +45,14 @@ class Order(CreateView):
     form_class = OrderForm
     template_name = "store/order.html"
 
-    # def get(self, request, *args, **kwargs):
-    #     now = datetime.date(datetime.now())
-    #     context = {"now": str(now),
-    #                "form": OrderForm,
-    #                }
-    #     print(context)
-    #     print(now)
-    #     return render(request, "store/order.html", context=context)
+    def get(self, request, *args, **kwargs):
+        now = datetime.date(datetime.now())
+        context = {"now": str(now),
+                   "form": OrderForm,
+                   }
+        print(context)
+        print(now)
+        return render(request, "store/order.html", context=context)
     def get_success_url(self):
         return reverse_lazy('main')
 class AddProduct(CreateView):
